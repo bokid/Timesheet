2325503b92197684c7ba4e082d643738ff59dcac
LAN Default <lfg@LFG.LOCAL>
2022-01-28 22:44:02 -0500

    Add 2 user functionality
    Add multiple inputs with player 1 being controlled by the keyboard and
    player 2 being controlled with a gamepad
    
    Implemented 'new input system'
    
    Implemented eventlistener for onMove

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
@@ -9,6 +10,7 @@ public class PlayerController : MonoBehaviour
     //public Animator animator;
     public float speed;
     private Rigidbody2D player;
+    private Vector2 movementInput = Vector2.zero;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,12 +20,13 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        horInput = UnityEngine.Input.GetAxisRaw("Horizontal");
-        vertInput = UnityEngine.Input.GetAxisRaw("Vertical");
+        horInput = movementInput.x;
+        vertInput = movementInput.y;
         //animator.SetFloat("Speed", Mathf.Abs(horInput));
-
         player.velocity = new Vector2(horInput * speed, vertInput * speed);
-       
-         
+    }
+
+    public void onMove(InputAction.CallbackContext context){
+        movementInput = context.ReadValue<Vector2>();
     }
 }
