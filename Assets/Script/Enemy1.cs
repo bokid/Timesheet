d4200efcc23e0500bbb6e159c516632a801057da
Ben Orr <benjaminbordwell@gmail.com>
2022-01-29 11:47:16 -0500

    Build enemy script that follows player. Build levelmanager, playerdeath and respawn, oncollision2d between enemy and player destroys player gameObject, respawns player1 prefab, updates enemies target to new player prefab

---
+++ Assets/Script/Enemy1.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy1 : MonoBehaviour
+{
+
+
+    // Start is called before the first frame update
+    public float speed;
+    private Transform target;
+    void Start()
+    {
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        
+        if (Vector2.Distance(transform.position, target.position) > 0)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        }
+
+    
+
+    }
+
+    
+}
