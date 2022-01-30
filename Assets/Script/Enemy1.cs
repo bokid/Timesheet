01c8a227d06e4291c0a6a687b2a62637a3dec6cc
LAN Default <lfg@LFG.LOCAL>
2022-01-29 20:26:32 -0500

    refactor respawn mechanic

--- Assets/Script/Enemy1.cs
+++ Assets/Script/Enemy1.cs
@@ -24,9 +24,6 @@ public class Enemy1 : MonoBehaviour
         {
             transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
         }
-
-    
-
     }
 
     
